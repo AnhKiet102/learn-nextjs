b80a8facd139e56ebc8065d39c7b1e9734eca497
Kiet Pham <kiet.pham@c2c-techhub.io>
2022-10-07 22:17:40 +0700

    SSG getStaticProps with typescript

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,13 +1,34 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next';
 import * as React from 'react';
-
 export interface PostListProps {
+  posts:any[]
 }
 
-export default function PostList (props: PostListProps) {
+export default function PostList ({posts}: PostListProps) {
   return (
     <div>
       Post list page
-      
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
+}
+
+export const getStaticProps: GetStaticProps<PostListProps> = async(
+  context: GetStaticPropsContext
+) => {
+  //Server side
+  // build time
+  console.log('Static props')
+  const response = await fetch('https://js-post-api.herokuapp.com/api/posts?_page=1');
+  const data = await response.json()
+  console.log(data)
+  return {
+    props: {
+      posts: data.data.map((x: any) => ({ id: x.id, title: x.title })),
+    },
+  };
 }
